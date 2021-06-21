72457ee344f02ad254c120d62d0ba3185e77ab6a
Moroco <lmoroco@unsa.edu.pe>
2021-06-21 01:14:07 -0500

    xd

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char const *argv[])
 {
     srand(time(NULL));
-    std::cout << "Ra!" << std::endl;
+    std::cout << "Raaaaa!" << std::endl;
 
     //Incializar
     std::vector<int> array = {6, 5, 1, 2, 4, 3, 0, 7};
